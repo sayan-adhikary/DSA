202fde35159fcf51d08b928ae1aaf2bd91dabe00
sayan-adhikary <pap9679429830@gmail.com>
2024-12-31 17:55:24 +0530

    Day-1

---
+++ largestElement.java
@@ -0,0 +1,23 @@
+public class largestElement {
+    static void largestElement(int []arr){
+        int largest = Integer.MIN_VALUE;
+        int smallest = Integer.MAX_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {              //TC = O(n) => loop n time
+            if(arr[i]>largest){
+                largest = arr[i];
+            }
+            if(arr[i]<smallest){
+                smallest = arr[i];
+            }
+    }
+    // return largest;
+    System.out.println("Largest value: "+ largest);
+    System.out.println("Smallest value: "+ smallest);
+    }
+
+    public static void main(String[] args) {
+        int []number = {1,2,3,6,4,6};
+        largestElement(number);
+    }
+}
