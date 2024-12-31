22a15aeda25d874a4091403e41b61e284ae0409e
sayan-adhikary <pap9679429830@gmail.com>
2024-12-31 13:23:23 +0530

    Day-1

---
+++ array.java
@@ -0,0 +1,36 @@
+class array{
+    // public static void main(String[] args) {
+
+        //Create
+
+        int arr[] = new int [5];
+        int marks[] = {2 , 3 , 6 , 7};
+        int moreMarks[] = {4 , 5 , 6};
+        String fruit[] = {"Apple" , "Banana" , "Mango" , "Orange"};
+
+        //Input
+        /* marks[0] = 21;
+        marks[1] = 22;
+        marks[2] = 23;
+        marks[3] = 24;
+        System.out.println(marks[0]+" "+marks[1]+" "+marks[2]+" "+marks[3]); */
+
+        //Passing Array using argument
+        static void Update(int marks[]){
+
+            for (int i = 0; i < marks.length; i++) {
+                marks[i] = marks[i] + 1;
+            }
+        }
+
+        public static void main(String[] args) {
+            int []marks = {2 , 3 , 4};
+            Update(marks);
+            for (int i = 0; i < marks.length; i++) {
+                System.out.print(marks[i]+" ");
+            }
+        }
+
+        
+
+    }
---
+++ liniarshearch.java
@@ -0,0 +1,21 @@
+public class liniarshearch {
+    public static int liniarshearch(int numbers[],int key){
+        for (int i = 0; i < numbers.length; i++) {
+            if(numbers[i] == key){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int numbers[]={2,3,4,5,6,7,8,9};
+        int key = 17;
+        int index = liniarshearch(numbers, key);
+        if(index == -1){
+            System.out.println("Not Found");
+        }else{
+            System.out.println("Key index is: "+ index);
+        }
+    }
+}
