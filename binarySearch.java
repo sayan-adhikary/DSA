2a9ab3ca4748fd886f08c83aa8d62eb051a1ce26
sayan-adhikary <pap9679429830@gmail.com>
2024-12-31 18:45:37 +0530

    Day-1

---
+++ binarySearch.java
@@ -0,0 +1,24 @@
+public class binarySearch {
+    public static int binarySearch(int []arr,int key){
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = (start + end)/2;
+            if (arr[mid] == key) {
+                return mid;
+            }
+            if(arr[mid] > key){
+                end = mid - 1;
+            }
+            if(arr[mid] < key){
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int []number = {2,4,6,8,10,12,14};
+        int key = 10;
+        System.out.println(binarySearch(number, key));
+      }
+}
