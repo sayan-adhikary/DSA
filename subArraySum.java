2318b6959489edfca1952cd1f9c50c4a1e244730
sayan-adhikary <pap9679429830@gmail.com>
2025-01-02 08:29:49 +0530

    Day-3

--- subArraySum.java
+++ subArraySum.java
@@ -1,26 +1,27 @@
 public class subArraySum {
         static void subArraySum(int arr[]){
-            int TN = 0;
-            //int currentSum = 0;
+            int max = Integer.MIN_VALUE;
+            int currentSum = 0;
+
             for (int i = 0; i < arr.length; i++) {              
                 int start = i;
                 for (int j = start; j < arr.length; j++) {
                     int end = j;
-                    int currentSum = 0;
+                    currentSum = 0;
                     for (int k = start; k <= end; k++) {
                          currentSum += arr[k];
                     }
-                    System.out.print(currentSum);
-                    TN++;
-                    System.out.println();
+                    System.out.println(currentSum);
+                if (max<currentSum) {
+                    max = currentSum;
                 }
-                System.out.println();
             }
-            System.out.println("Total Number: "+ TN);
         }
+        System.out.println("MAX VALUE: "+ max);
+    }
         public static void main(String[] args) {
             int number[] = {2,4,6,8,10};
-            subArray(number);
+            subArraySum(number);
         }
     } 
 
