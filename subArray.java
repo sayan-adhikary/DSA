8cc73cd529215d7a63348af022c0929d7c37c632
sayan-adhikary <pap9679429830@gmail.com>
2025-01-02 07:56:14 +0530

    Day-2

--- subArray.java
+++ subArray.java
@@ -1,16 +1,19 @@
 public class subArray {
     static void subArray(int arr[]){
+        int TN = 0;
         for (int i = 0; i < arr.length; i++) {
             int start = i;
-            for (int j = 0; j < arr.length; j++) {
+            for (int j = start; j < arr.length; j++) {
                 int end = j;
-                for (int k = 0; k < arr.length; k++) {
+                for (int k = start; k <= end; k++) {
                     System.out.print(arr[k]+" ");
                 }
+                TN++;
                 System.out.println();
             }
             System.out.println();
         }
+        System.out.println("Total Number: "+ TN);
     }
     public static void main(String[] args) {
         int number[] = {2,4,6,8,10};
