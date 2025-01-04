44b0e8b8d1a541ba83e7109333cd038b2f569446
sayan-adhikary <pap9679429830@gmail.com>
2025-01-04 22:11:06 +0530

    Day-4

---
+++ buyAndSellStock.java
@@ -0,0 +1,21 @@
+public class buyAndSellStock {
+    static void buyAndSellStock(int []prices){      //TC=O(n)
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            if(buyPrice < prices[i]){       //profit
+                int profit = prices[i] - buyPrice;
+                maxProfit = Math.max(maxProfit, profit);
+            } else{
+                buyPrice = prices[i];  
+            }
+        }
+        System.out.println(maxProfit);
+    }
+
+    public static void main(String[] args) {
+        int []prices = {7,1,5,3,6,4}; 
+        buyAndSellStock(prices);
+    }
+}
