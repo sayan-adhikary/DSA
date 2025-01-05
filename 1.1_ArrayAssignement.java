afe778ab84bf3c4a44045d772eada4794df9caf3
sayan-adhikary <pap9679429830@gmail.com>
2025-01-05 08:38:05 +0530

    Day-5

---
+++ 1.1_ArrayAssignement.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class array{
+    public static boolean containsDuplicate(int[] nums) {
+            // Sort the array first
+            Arrays.sort(nums);
+            
+            // Check adjacent elements for duplicates
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] == nums[i-1]) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    public static void main(String[] args) {
+        int []nums = {1,2,3,1};
+        System.out.println(containsDuplicate(nums));
+    }
+}
