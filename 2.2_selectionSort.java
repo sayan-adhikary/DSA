5186cd63ea81025d57a8377efe6d315654c8bf2f
sayan-adhikary <pap9679429830@gmail.com>
2025-01-06 11:50:05 +0530

    Day-5

---
+++ 2.2_selectionSort.java
@@ -0,0 +1,25 @@
+class Sort {
+    static void selectionSort(int[] arr){
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minPos = i;
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[minPos] > arr[j]) {
+                    minPos = j;
+                }
+            }
+            //swap
+            if(minPos != i){
+            int temp = arr[minPos];
+            arr[minPos] = arr[i];
+            arr[i] = temp;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {5,4,3,2,1};
+        selectionSort(arr);
+        for (int i : arr) {
+            System.out.print(i+" ");
+        }
+    }
+}
