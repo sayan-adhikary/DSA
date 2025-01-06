50ce9df16408b9c48342f35ed9baf287c7b52297
sayan-adhikary <pap9679429830@gmail.com>
2025-01-06 13:05:55 +0530

    Day-6

---
+++ 2.5_inbuildSorting.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+class inbuildSorting {
+    public static void main(String[] args) {
+        Integer arr[] = {5,3,5,2,8,9,2,1};
+        Arrays.sort(arr);                                    //o/p:- 1 2 2 3 5 5 8 9 
+        // Arrays.sort(arr,0,3);                                //o/p:- 3 5 5 2 8 9 2 1
+        // Arrays.sort(arr,Collections.reverseOrder());         //o/p:- 9 8 5 5 3 2 2 1 
+        // Arrays.sort(arr,0,3,Collections.reverseOrder());     //o/p:- 5 5 3 2 8 9 2 1
+        for (Integer i : arr) {
+            System.out.print(i+" ");
+        }
+    }
+}
