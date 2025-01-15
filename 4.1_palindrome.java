96a6d2dd63a1a5fe42351f527366aac88ad4836b
sayan-adhikary <pap9679429830@gmail.com>
2025-01-15 18:33:04 +0530

    Day-9

--- 4.1_palindrome.java
+++ 4.1_palindrome.java
@@ -1,33 +1,33 @@
-class Palindrome {
-    static void checkPalindrome(String str) {
-        int start = 0;
-        int end = str.length() - 1;
+class Palindrome {                                      //Both methods TC: O(n) SC: O(1)
+    static void checkPalindrome(String str) {           // Function to check palindrome
+        int start = 0;                        // Start index
+        int end = str.length() - 1;             // End index
 
         // Loop to compare characters from both ends
-        while (start < end) {
-            if (str.charAt(start) != str.charAt(end)) {
-                System.out.println("Not Palindrome");
+        while (start < end) {              // While start is less than end
+            if (str.charAt(start) != str.charAt(end)) {     // If characters are not equal
+                System.out.println("Not Palindrome");           // Print Not Palindrome
                 return;  // No need to check further if it's already not a palindrome
             }
-            start++;
-            end--;
-        }
+            start++;                                        // Increment start
+            end--;                                        // Decrement end                          
+        }                                                   // End of loop
         // If the loop completes, it's a palindrome
-        System.out.println("Palindrome");
+        System.out.println("Palindrome");                   // Print Palindrome
     }
 
-    static boolean palindrome(String str){
-       for(int i = 0; i < str.length()/2; i++) {
-        int n = str.length();
-        if (str.charAt(i) != str.charAt(n - i - 1)) {
+    static boolean palindrome(String str){              // Function to check palindrome
+       for(int i = 0; i < str.length()/2; i++) {        // Loop to compare characters from both ends
+        int n = str.length();                       // Length of string
+        if (str.charAt(i) != str.charAt(n - i - 1)) {       // If characters are not equal
             return false;  // No need to check further if it's already not a palindrome
         }
-         }
-        return true;
+        }
+        return true;                                    // If the loop completes, it's a palindrome
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) {                // Main method
         String str = "amltma";  // You can change the string for testing
         // checkPalindrome(str);
-        System.out.println(palindrome(str));
+        System.out.println(palindrome(str));        // Function call
     }
 }
