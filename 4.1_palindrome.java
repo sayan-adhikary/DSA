0e41f73845fc93033ad5a3a4b741a8bfc3c2f4fb
sayan-adhikary <pap9679429830@gmail.com>
2025-01-15 18:29:54 +0530

    Day-9

--- 4.1_palindrome.java
+++ 4.1_palindrome.java
@@ -12,13 +12,22 @@ class Palindrome {
             start++;
             end--;
         }
-
         // If the loop completes, it's a palindrome
         System.out.println("Palindrome");
     }
 
+    static boolean palindrome(String str){
+       for(int i = 0; i < str.length()/2; i++) {
+        int n = str.length();
+        if (str.charAt(i) != str.charAt(n - i - 1)) {
+            return false;  // No need to check further if it's already not a palindrome
+        }
+         }
+        return true;
+    }
     public static void main(String[] args) {
-        String str = "amlma";  // You can change the string for testing
-        checkPalindrome(str);
+        String str = "amltma";  // You can change the string for testing
+        // checkPalindrome(str);
+        System.out.println(palindrome(str));
     }
 }
