dd74918ba7e42cdc3274e3612052248d57511cb5
sayan-adhikary <pap9679429830@gmail.com>
2025-05-19 12:17:15 +0530

    New day-1

--- 5.2_getposition.java
+++ 5.2_getposition.java
@@ -1,5 +1,5 @@
 class getposition {
-    public static void printBitAtPosition(int n, int i){
+    public static void getIthBit(int n, int i){
         int bitmask = 1 << i;
         if((n & bitmask) == 0) {
             System.out.println("0");
@@ -8,10 +8,28 @@ class getposition {
         }
     }
 
+    public static void setIthBit(int n, int i){
+        int bitmask = 1 << i;
+        int newNumber = n | bitmask;
+        System.out.println(newNumber);
+    }
+
+    public static void clearIthBit(int n, int i){
+        int bitmask = 1 << i;
+        int newNumber = n & ~bitmask;
+        System.out.println(newNumber);
+    }
+
     public static void main(String[] args) {
-        printBitAtPosition(5, 3); // 0
-        printBitAtPosition(5, 2); // 0
-        printBitAtPosition(5, 1); // 1
-        printBitAtPosition(5, 0); // 1
+        getIthBit(5, 3); // 0
+        getIthBit(5, 2); // 0
+        getIthBit(5, 1); // 1
+        getIthBit(5, 0); // 1
+
+        setIthBit(5, 1); // 7
+        setIthBit(5, 2); // 5
+
+        clearIthBit(5, 1); // 4
+        clearIthBit(10, 1); // 1
     }
 }
