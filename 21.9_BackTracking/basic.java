802103a0a0d2617409734eedc7f61c968e4721d2
sayan-adhikary <pap9679429830@gmail.com>
2025-08-04 11:52:16 +0530

    Update basic.java

--- 21.9_BackTracking/basic.java
+++ 21.9_BackTracking/basic.java
@@ -13,7 +13,7 @@ public class basic {
 
     public static void printArr(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i] + " ");
+            System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
