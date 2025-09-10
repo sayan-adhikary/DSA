7d0cee8815a0f5d9a92d12b0ec498de475bcc76c
sayan-adhikary <103528635+sayan-adhikary@users.noreply.github.com>
2025-09-10 10:53:08 +0530

    Update 11.1_LinkedList.java

--- 22.20_Linked_List/11.1_LinkedList.java
+++ 22.20_Linked_List/11.1_LinkedList.java
@@ -11,11 +11,13 @@ class LinkedList {
 
     public static Node head;
     public static Node tail;
+    public static int size;
 
-    public void addFirst(int data) {    //addFirst
+    public void addFirst(int data) { // addFirst
 
         // step1 = creat new node
         Node newNode = new Node(data);
+        size++;
         if (head == null) {
             head = tail = newNode;
             return;
@@ -28,8 +30,9 @@ class LinkedList {
         head = newNode;
     }
 
-    public void addLast(int data) {     //addLast
+    public void addLast(int data) { // addLast
         Node newNode = new Node(data);
+        size++;
         if (head == null) {
             head = tail = newNode;
             return;
@@ -38,7 +41,139 @@ class LinkedList {
         tail = newNode;
     }
 
-    public void print() {       //print
+    public void add(int idx, int data) { // addMiddle
+        if (idx == 0) {
+            addFirst(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        size++;
+        Node temp = head;
+        int i = 0;
+
+        while (i < idx - 1) {
+            temp = temp.next;
+            i++;
+        }
+        // i = idx - 1; temp -> prev
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
+    public int removeFirst() { // removeFirst
+        if (size == 0) {
+            System.out.println("LL is empty");
+            return Integer.MIN_VALUE;
+        } else if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        int val = head.data;
+        size--;
+        head = head.next;
+        return val;
+    }
+
+    public int removeLast() { // removeLast
+        if (size == 0) {
+            System.out.println("LL is empty");
+            return Integer.MIN_VALUE;
+        } else if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        Node prev = head;
+        for (int i = 0; i < size - 2; i++) {
+            prev = prev.next;
+        }
+        int val = tail.data; // prev.next.data;
+        prev.next = null;
+        tail = prev;
+        size--;
+        return val;
+    }
+
+    public int itrSearch(int key) { // search Element O(n)
+        Node temp = head;
+        int i = 0;
+
+        while (temp != null) {
+            // key found
+            if (temp.data == key) {
+                return i;
+            }
+            temp = temp.next;
+            i++;
+        }
+        // key not found
+        return -1;
+    }
+
+    public int helper(Node head, int key) { // O(n) recSearch
+        if (head == null) {
+            return -1;
+        }
+
+        if (head.data == key) {
+            return 0;
+        }
+        int idx = helper(head.next, key);
+        if (idx == -1) {
+            return -1;
+        }
+        return idx + 1;
+    }
+
+    public int recSearch(int key) { // using helperFunction
+        return helper(head, key);
+    }
+
+    public void reverse() { // reverse O(n)
+        Node prev = null;
+        Node curr = tail = head;
+        Node next;
+
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+    }
+
+    public void deleteNthfromEnd(int n) {       //Delete the N-th Node from the End O(n)
+        // calculate size
+        int sz = 0;
+        Node temp = head;
+        // Step 1: Calculate size
+        while (temp != null) {
+            temp = temp.next;
+            sz++;
+        }
+        //Step 2: Check if n == sz
+        if (n == sz) {
+            head = head.next; // removeFirst
+            return;
+        }
+
+        //Step 3: Find node before the one to delete [sz-n]
+        int i = 1;
+        int iToFind = sz - n;
+        Node prev = head;
+        while (i < iToFind) {
+            prev = prev.next;
+            i++;
+        }
+        prev.next = prev.next.next;
+        return;
+    }
+
+    public void print() { // print
         if (head == null) {
             System.out.println("Empty LinkedList");
             return;
@@ -53,14 +188,31 @@ class LinkedList {
 
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
-        ll.print();
+        // ll.print();
         ll.addFirst(2);
-        ll.print();
+        // ll.print();
         ll.addFirst(1);
-        ll.print();
+        // ll.print();
         ll.addLast(3);
-        ll.print();
+        // ll.print();
         ll.addLast(4);
+        // ll.print();
+        // ll.add(2, 9);
+        // ll.print();
+        // ll.removeFirst();
+        // ll.print();
+        // ll.removeLast();
+        ll.print();
+
+        // ll.reverse();
+        // ll.print();
+
+        // System.out.println(ll.itrSearch(9));
+        // System.out.println(ll.recSearch(9));
+
+        // System.out.println(ll.size);
+
+        ll.deleteNthfromEnd(2);
         ll.print();
     }
 }
