2180aed806b061f3c566ab406b33f1da1551035c
sayan-adhikary <103528635+sayan-adhikary@users.noreply.github.com>
2025-11-24 11:20:32 +0530

    Updated

---
+++ 25.13_Stack/13.2_stackLinkedList.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class stackA {
+    static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    static class Static{
+        static Node head = null;
+
+        public static boolean isEmpty(){
+            return head = null;
+        }
+    }
+    
+}
