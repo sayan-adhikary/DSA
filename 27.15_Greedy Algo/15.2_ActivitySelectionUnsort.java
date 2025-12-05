24c03909c3787649083e31d2c585f6a75b0c448c
sayan-adhikary <103528635+sayan-adhikary@users.noreply.github.com>
2025-12-05 12:12:12 +0530

    updated

---
+++ 27.15_Greedy Algo/15.2_ActivitySelectionUnsort.java
@@ -0,0 +1,43 @@
+import java.util.*;
+class ActivitySelectionUnsort {
+    
+    public static void main(String[] args) {
+
+        int start[] = {1, 3, 0, 5, 8, 5};
+        int end[]   = {2, 4, 6, 7, 9, 9};
+
+        // Structure: [ index, start, end ]
+        int activities[][] = new int[start.length][3];
+
+        for (int i = 0; i < start.length; i++) {
+            activities[i][0] = i;        // index
+            activities[i][1] = start[i]; // start time
+            activities[i][2] = end[i];   // end time
+        }
+
+        // Sort by end time using your lambda
+        Arrays.sort(activities, Comparator.comparingDouble(o -> o[2]));
+
+        // Activity Selection
+        ArrayList<Integer> ans = new ArrayList<>();
+        int count = 1;
+
+        ans.add(activities[0][0]); // add index of first selected activity
+        int lastEnd = activities[0][2];
+
+        for (int i = 1; i < activities.length; i++) {
+            if (activities[i][1] >= lastEnd) {
+                count++;
+                ans.add(activities[i][0]); // original index
+                lastEnd = activities[i][2];
+            }
+        }
+
+        // Output
+        System.out.println("Max activities = " + count);
+        for (int idx : ans) {
+            System.out.print("A" + idx + " ");
+        }
+        System.out.println();
+    }
+}
