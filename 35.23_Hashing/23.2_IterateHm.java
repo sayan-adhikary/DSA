96046042789bd7456d37e491fdd445a62f466e8d
sayan-adhikary <103528635+sayan-adhikary@users.noreply.github.com>
2025-12-12 13:27:45 +0530

    Update 23.2_IterateHm.java

--- 35.23_Hashing/23.2_IterateHm.java
+++ 35.23_Hashing/23.2_IterateHm.java
@@ -4,7 +4,7 @@ class IterateHm {
         HashMap <String, Integer> hm = new HashMap<>();
         hm.put("India", 100);
         hm.put("Australia", 120);
-        hm.put("Afrika", 130);
+        hm.put("Afrika", 230);
         hm.put("China", 220);
         hm.put("America", 20);
 
@@ -12,9 +12,16 @@ class IterateHm {
         //hm.entrySet()
         Set<String> keys = hm.keySet();
         System.out.println(keys);
-        for (String str : keys) {
-            System.out.println("Key= "+str +", value= "+ hm.get(str));
+        int max = Integer.MIN_VALUE;
+        String maxKey = "";
+        for (String k : keys) {
+            // System.out.println("Key= "+str +", value= "+ hm.get(str));
+            if (max < hm.get(k)) {
+                max = hm.get(k);
+                maxKey = k;
+            }
         }
+        System.out.println(maxKey+" "+max);
         
     }
 }
