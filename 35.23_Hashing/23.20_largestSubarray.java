d175604204f63f8a38dc0adfc4fd0dc5f7875084
sayan-adhikary <103528635+sayan-adhikary@users.noreply.github.com>
2026-01-08 11:56:08 +0530

    Update 23.20_largestSubarray.java

--- 35.23_Hashing/23.20_largestSubarray.java
+++ 35.23_Hashing/23.20_largestSubarray.java
@@ -8,6 +8,9 @@ class largestSubarray {
         int length = 0;
         for (int j = 0; j < nums.length; j++) {
             sum+=nums[j];
+            if(sum == 0){
+                length = j + 1;        //0 -> j all value will be 0
+            }
             if(hm.containsKey(sum)){
                 length = Math.max(length, j-hm.get(sum));
             } else{
@@ -17,8 +20,13 @@ class largestSubarray {
         return length;
     }
 
+
     public static void main(String[] args) {
         int[] arr = { 15, -2, 2, -8, 1, 7, 10, 23 };
+        int[] arr1 = { -31, -48, -90, 54, 20, 95, 6, -86, 22 };  //op-6  
+        int[] arr2 = {-72, 68, -88, -7, 80, -80, 99, 21};   //op-7
         System.out.println(largestSarr(arr));
+        System.out.println(largestSarr(arr1));
+        System.out.println(largestSarr(arr2));
     }
 }
