23d3aebc804a299893b8750c3a4a0b9c9beceade
sayan-adhikary <103528635+sayan-adhikary@users.noreply.github.com>
2026-02-11 12:47:04 +0530

    Update 15.10_jobSequenceProblem.java

--- 27.15_Greedy Algo/15.10_jobSequenceProblem.java
+++ 27.15_Greedy Algo/15.10_jobSequenceProblem.java
@@ -1,4 +1,24 @@
 import java.util.*;
 class jobSequenceProblem {
-    
+    static class job{
+        int dadeline;
+        int profit;
+        int id;
+
+        public job(int i, int d, int p){
+            id = i;
+            dadeline = d;
+            profit = p;
+        }
+    }
+
+    public static void main(String[] args) {
+        int jobInfo[][] = {{4,20}, {1,10},{1, 40},{1, 30}};
+
+        ArrayList<job> jobs = new ArrayList<>();
+
+        for (int i = 0; i < jobInfo.length; i++) {
+            jobs.add(new job(i, jobInfo[i][0], jobInfo[i][1]));
+        }
+    }
 }
