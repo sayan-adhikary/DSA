78212439aab2c860e12b8bd8613c0af5877e0a1c
sayan-adhikary <103528635+sayan-adhikary@users.noreply.github.com>
2026-02-11 12:18:23 +0530

    problem :- Indian coins

---
+++ 27.15_Greedy Algo/15.9_indianCoin.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class indianCoin {
+    public static void printCoin(int amount) {                                      //TC-O(n) SC-O(n)  [for ArrayList space is n]
+        int count = 0;
+        Integer[] coins = { 1, 2, 5, 10, 20, 50, 100, 500, 2000 };
+
+        Arrays.sort(coins, Comparator.reverseOrder());                              // decending order of coins
+
+        ArrayList<Integer> ans = new ArrayList<>();                                 //store the coins
+
+        for (int i = 0; i < coins.length; i++) {
+            if (coins[i] <= amount) {                                               // amount should check if it is greater then go inside
+
+                while (coins[i] <= amount) {                                        // if coins is less than amount
+                    count++;                                                        // count incress
+                    ans.add(coins[i]);
+                    amount -= coins[i];                                             // amount will be subtract by coin
+                }
+            }
+        }
+        System.out.println(ans);
+        System.out.println(ans.size());
+        System.out.println(count);
+    }
+
+    public static void main(String[] args) {
+        int value = 590;
+        // System.out.println(printCoin(value));
+        printCoin(value);
+    }
+}
