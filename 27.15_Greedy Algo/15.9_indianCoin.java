0bb1c425997e99444509618d841b84788a85107c
sayan-adhikary <103528635+sayan-adhikary@users.noreply.github.com>
2026-02-12 13:14:38 +0530

    Update 15.9_indianCoin.java

--- 27.15_Greedy Algo/15.9_indianCoin.java
+++ 27.15_Greedy Algo/15.9_indianCoin.java
@@ -1,27 +1,27 @@
 import java.util.*;
 
 class indianCoin {
-    public static void printCoin(int amount) {                                      //TC-O(n) SC-O(n)  [for ArrayList space is n]
+    public static void printCoin(int amount) {                     //TC-O(n) SC-O(n)  [for ArrayList space is n]
         int count = 0;
         Integer[] coins = { 1, 2, 5, 10, 20, 50, 100, 500, 2000 };
 
-        Arrays.sort(coins, Comparator.reverseOrder());                              // decending order of coins
+        Arrays.sort(coins, Comparator.reverseOrder());             // decending order of coins
 
-        ArrayList<Integer> ans = new ArrayList<>();                                 //store the coins
+        ArrayList<Integer> ans = new ArrayList<>();                //store the coins
 
-        for (int i = 0; i < coins.length; i++) {                                    // iterate over the coins
-            if (coins[i] <= amount) {                                               // amount should check if it is greater then go inside
+        for (int i = 0; i < coins.length; i++) {                   // iterate over the coins
+            if (coins[i] <= amount) {                              // amount should check if it is greater then go inside
 
-                while (coins[i] <= amount) {                                        // if coins is less than amount
-                    count++;                                                        // count incress
+                while (coins[i] <= amount) {                       // if coins is less than amount
+                    count++;                                       // count incress
                     ans.add(coins[i]);
-                    amount -= coins[i];                                             // amount will be subtract by coin
+                    amount -= coins[i];                            // amount will be subtract by coin
                 }
             }
         }
-        System.out.println(ans);                                                    // print the coins
-        System.out.println(ans.size());                                             // print the count of coins                       
-        System.out.println(count);                                                  // print the count of coins
+        System.out.println(ans);                                   // print the coins
+        System.out.println(ans.size());                            // print the count of coins                       
+        System.out.println(count);                                 // print the count of coins
     }
 
     public static void main(String[] args) {
