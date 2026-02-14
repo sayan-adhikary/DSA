bb729c6ce35fc010d8af2b52aa02ff73f0bb781b
sayan-adhikary <103528635+sayan-adhikary@users.noreply.github.com>
2026-02-14 09:37:44 +0530

    Update 15.11_chocolateProblem.java

--- 27.15_Greedy Algo/15.11_chocolateProblem.java
+++ 27.15_Greedy Algo/15.11_chocolateProblem.java
@@ -2,31 +2,31 @@ import java.util.*;
 
 class chocolateProblem {
     public static void minCost(Integer costVer[], Integer costHor[], int m, int n) {
-        Arrays.sort(costVer, Collections.reverseOrder());
-        Arrays.sort(costHor, Collections.reverseOrder());
+        Arrays.sort(costVer, Collections.reverseOrder());                               // Sort the vertical and horizontal cost arrays in descending order
+        Arrays.sort(costHor, Collections.reverseOrder());                               // This ensures that we always consider the most expensive cut first, which is a greedy approach to minimize the total cost.
 
         int h = 0, v = 0;
         int hp = 1, vp = 1;
         int cost = 0;
 
-        while (h < costHor.length && v < costVer.length) {
-            if (costVer[v] <= costHor[h]) {
-                cost += (costHor[h] * vp);
+        while (h < costHor.length && v < costVer.length) {                              // Iterate through both cost arrays until we have processed all cuts
+            if (costVer[v] <= costHor[h]) {                                             // If the current horizontal cut cost is greater than or equal to the current vertical cut cost, we choose the horizontal cut     
+                cost += (costHor[h] * vp);                                              // The cost of the horizontal cut is multiplied by the number of vertical pieces (vp) because each horizontal cut will affect all vertical pieces
                 hp++;
                 h++;
-            } else {
-                cost += (costVer[v] * hp);
+            } else {                                                                    // If the current vertical cut cost is greater than the current horizontal cut cost, we choose the vertical cut 
+                cost += (costVer[v] * hp);                                              // The cost of the vertical cut is multiplied by the number of horizontal pieces (hp) because each vertical cut will affect all horizontal pieces
                 vp++;
                 v++;
             }
         }
-        while (h < costHor.length) {
-            cost += (costHor[h] * vp);
+        while (h < costHor.length) {                                                    // If there are remaining horizontal cuts, we process them
+            cost += (costHor[h] * vp);                                                  // Each remaining horizontal cut will affect all vertical pieces (vp)   
             hp++;
             h++;
         }
-        while (v < costVer.length) {
-            cost += (costVer[v] * hp);
+        while (v < costVer.length) {                                                    // If there are remaining vertical cuts, we process them                        
+            cost += (costVer[v] * hp);                                                  // Each remaining vertical cut will affect all horizontal pieces (hp)                             
             vp++;
             v++;
         }
@@ -56,3 +56,14 @@ class chocolateProblem {
         minCost(costVer, costHor, m, n);
     }
 }
+
+
+/*
+Input :- 
+6 4
+2 1 3 1 4
+4 1 2
+
+Output:
+42
+ */
